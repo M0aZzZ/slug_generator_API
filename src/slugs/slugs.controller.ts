65a79e889c94412e6c76636252bc28e9e7a26658
m0az <moazkm2@gmail.com>
2026-05-25 13:59:26 +0300

    add primitve crud for slugs

--- src/slugs/slugs.controller.ts
+++ src/slugs/slugs.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Get, Delete, Body, Param } from '@nestjs/common';
 import { SlugsService } from './slugs.service';
+import type { slug } from './slug.model';
 
 @Controller('slug')
 export class SlugsController {
   constructor(private readonly SlugsService: SlugsService) {}
-
+  @Post()
+  createSlug(@Body() newSlug: slug): string {
+    return this.SlugsService.createSlug(newSlug.originalString).slug;
+  }
   @Get()
-  generateTestSlug() {
-    return this.SlugsService.generateSlug('Hello World');
+  getSlugs() {
+    return this.SlugsService.getSlugs();
+  }
+  @Get(':id')
+  getSlugById(@Param('id') id: string): slug | undefined {
+    return this.SlugsService.getSlugById(parseInt(id));
+  }
+  @Delete(':id')
+  deleteSlug(@Param('id') id: string): void {
+    this.SlugsService.deleteSlug(parseInt(id));
   }
 }
--- src/slugs/slugs.service.ts
+++ src/slugs/slugs.service.ts
@@ -4,11 +4,24 @@ import { slug } from './slug.model';
 
 @Injectable()
 export class SlugsService {
-  generateSlug(originalString: string): slug {
-    return {
-      id: 0,
+  private slugs: slug[] = [];
+
+  createSlug(originalString: string): slug {
+    const newSlug: slug = {
+      id: this.slugs.length + 1,
       originalString,
       slug: slugify(originalString),
     };
+    this.slugs.push(newSlug);
+    return newSlug;
+  }
+  getSlugs(): slug[] {
+    return this.slugs;
+  }
+  getSlugById(id: number): slug | undefined {
+    return this.slugs.find((slugs) => slugs.id === id);
+  }
+  deleteSlug(id: number): void {
+    this.slugs = this.slugs.filter((slugs) => slugs.id !== id);
   }
 }
